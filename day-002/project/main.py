fb84f603a5fd9290be67ae3db0379aadfe41ceb2
caverney-cave <jazulidesign00@gmail.com>
2026-02-13 16:33:07 +0700

    Editing line 10

--- day-002/project/main.py
+++ day-002/project/main.py
@@ -7,5 +7,4 @@ people_sum = int(input("How many people to split the bill? "))
 tip_sum = total_bill * tip / 100
 should_pay = ((total_bill + tip_sum) / people_sum)
 
-# print(f"Each person should pay: {round(should_pay, 2)}")
-print(tip_sum)
+print(f"Each person should pay: ${round(should_pay, 2)}")
